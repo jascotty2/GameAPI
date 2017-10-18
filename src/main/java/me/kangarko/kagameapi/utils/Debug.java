f9c984137b163b4f6c37acfcd245496b11b8d5af
kangarko <kangaro.matej@azet.sk>
2017-10-18 19:27:55 +0200

    Add Debug

---
+++ src/main/java/me/kangarko/kagameapi/utils/Debug.java
@@ -0,0 +1,24 @@
+package me.kangarko.kagameapi.utils;
+
+/**
+ * Utility class
+ */
+public class Debug {
+
+	/**
+	 * Traces method calls from the invocation place.
+	 *
+	 * @param message the title of the message for clarity
+	 */
+	public static final void trace(String message) {
+		System.out.println("------------------------------------------------------------------------------------------------------------");
+		System.out.println(message);
+		System.out.println("");
+
+		for (final StackTraceElement e : new Throwable().getStackTrace())
+			if (e.toString().contains("me.kangarko"))
+				System.out.println("    at " + e.toString());
+
+		System.out.println("------------------------------------------------------------------------------------------------------------");
+	}
+}
